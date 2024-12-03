c4bcb05d33d69aedbeb74e8eb34f6e1a6c18ebbd
Plescan Andrei Leonard <leonardplescan@gmail.com>
2024-12-03 21:22:58 +0100

    aggiunto react-router, routes, ecc

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import "./App.css";
 // Importo lo stile di react-bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Importo il BrowserRoute da react-router-dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // Importo i componenti utilizzati
 import Navbar from "./component/Navbar/Navbar";
 import UsersPage from "./component/UsersPage.jsx/UsersPage";
 import Footer from "./component/Footer/Footer";
+import UserProfile from "./component/UserProfile/UserProfile";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <UsersPage />
+      <Routes>
+        <Route path="/" element={<UsersPage />} />
+        <Route path="/profile/:id" element={<UserProfile />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/component/UsersPage.jsx/UsersPage.jsx
+++ src/component/UsersPage.jsx/UsersPage.jsx
@@ -3,22 +3,48 @@ import { Col, Image, Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import FemaleImg from "../assets/img/female.png";
 import MaleImg from "../assets/img/male.png";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function UsersPage() {
+    
+  const navigate = useNavigate();
+
+  const usersID = {
+    jessica: "674f52c122ca0300154884b1",
+    carmine: "674e09d774b77d0015d3f07a",
+    gabriele: "674f538122ca0300154884b2",
+    andrei: "674e072574b77d0015d3f076",
+  };
+
   return (
     <Container>
-      <Row>
-        <Col xs={6} md={4}>
-          <Image src={FemaleImg} roundedCircle />
+      <Row className="text-center mt-5 g-5 vh-75">
+        <Col xs={12} md={6}>
+          <Link to={navigate(`/profile/${usersID.jessica}`)}>
+            <Image src={FemaleImg} roundedCircle />
+          </Link>
+          <p className="mt-4">Jessica Fraino</p>
         </Col>
-        <Col xs={6} md={4}>
-          <Image src={MaleImg} roundedCircle />
+
+        <Col xs={12} md={6}>
+          <Link to={navigate(`/profile/${usersID.andrei}`)}>
+            <Image src={MaleImg} roundedCircle />
+          </Link>
+          <p className="mt-4">Plescan Andrei Leonard</p>
         </Col>
-        <Col xs={6} md={4}>
-          <Image src={MaleImg} roundedCircle />
+
+        <Col xs={12} md={6}>
+          <Link to={navigate(`/profile/${usersID.gabriele}`)}>
+            <Image src={MaleImg} roundedCircle />
+          </Link>
+          <p className="mt-4">Gabriele Aloi</p>
         </Col>
-        <Col xs={6} md={4}>
-          <Image src={MaleImg} roundedCircle />
+
+        <Col xs={12} md={6}>
+          <Link to={navigate(`/profile/${usersID.carmine}`)}>
+            <Image src={MaleImg} roundedCircle />
+          </Link>
+          <p className="mt-4">Carmine Berardi</p>
         </Col>
       </Row>
     </Container>
