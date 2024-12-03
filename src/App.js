8e674ba7c0b6ef609c398b7ad09ef5213aa94069
Plescan Andrei Leonard <leonardplescan@gmail.com>
2024-12-03 19:28:15 +0100

    correzioni piccoli bug

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
+// Importo lo stile da App.css
 import "./App.css";
+
+// Importo lo stile di react-bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importo i componenti utilizzati
 import Navbar from "./component/Navbar/Navbar";
 import UsersPage from "./component/UsersPage.jsx/UsersPage";
-=======
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavbar from './Components/MyNavbar/MyNavbar.component';
-import MyFooter from './Components/MyFooter/MyFooter.components';
-=======
->>>>>>> 5364184673d00907d840f260ccefd24e8d48c370
->>>>>>> 5e6cd096ac09c188d4886503c51a1a3eb6b5f12d
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <Navbar />
       <UsersPage />
+      <Footer />
     </>
-=======
-<<<<<<< HEAD
-    <div>
-    <MyNavbar />
-    <MyFooter />
-    </div>
-=======
-   <>
-   <Navbar/>
-   </>
->>>>>>> 5364184673d00907d840f260ccefd24e8d48c370
->>>>>>> 5e6cd096ac09c188d4886503c51a1a3eb6b5f12d
   );
 }
 
